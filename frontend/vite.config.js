55563d6b201c86f1c5f70cb539f66cf0609c84ff
RachaponKjr <rachapon.navy@gmail.com>
2025-03-01 19:17:46 +0700

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,4 +15,4 @@ export default defineConfig({
     origin: "http://0.0.0.0:3011",
   },
   plugins: [react()],
-})
+});
