8b98d9c3fe3520cf7930a0e3836ee12d644de4ea
RachaponKjr <rachapon.navy@gmail.com>
2025-03-01 19:14:39 +0700

    fix

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port : 5174},
-})
+  base: "/",
+  preview: {
+    port: 3010,
+    strictPort: true,
+  },
+  server: {
+    port: 3010,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:3010",
+  },
+});
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
+  preview: {
+    port: 3011,
+    strictPort: true,
+  },
+  server: {
+    port: 3011,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:3011",
+  },
   plugins: [react()],
-  server: {port : 5173}
 })
